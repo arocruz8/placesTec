4418fcda428b32967dbf4cba7107052ca425ad27
Gilberto Rodriguez <Gilberto Rodriguez@192.168.1.2>
2018-06-14 10:39:43 -0600

    le agregue la varibele del ABB a los construcctores de la clase

--- src/main/java/grafo/NodoVertice.java
+++ src/main/java/grafo/NodoVertice.java
@@ -1,22 +1,25 @@
 package grafo;
-
+import árbolBB.*;
 import java.util.*;
 
 public class NodoVertice{
     public int vertice;                 //Almacena el dato del vértice
     public boolean visitado;            //Indica si el vértice ha sido visitado o no
     public LinkedList <Nodo> aristas;   //Almacena la lista de aristas del vértice
-
+    ABB arbolito;
+    
     public NodoVertice(){
         this.vertice = 0;
         this.visitado = false;
         this.aristas  = new LinkedList<Nodo>();
+        this.arbolito = new ABB();
     }//fin del constructor Nodo
 
     public NodoVertice(int v){
-            this.vertice = v;
-            this.visitado = false;
-            this.aristas  = new LinkedList<Nodo>();
+        this.vertice = v;
+        this.visitado = false;
+        this.aristas  = new LinkedList<Nodo>();
+        this.arbolito = new ABB();
     }//fin del constructor Nodo
 
     public String aCadena(){
