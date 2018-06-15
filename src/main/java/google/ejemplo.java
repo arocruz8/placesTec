d9d2733de4c9b5c26e4446288aebde6ac6ce65aa
Gilberto Rodriguez <Gilberto Rodriguez@192.168.1.2>
2018-06-15 13:56:26 -0600

    arregle codigo del ejemplo istale toda la puta mierda que decia la librebria y sigue sin funcionar esta grandisima picha

--- src/main/java/google/ejemplo.java
+++ src/main/java/google/ejemplo.java
@@ -6,10 +6,15 @@ import com.google.gson.GsonBuilder;
 import com.google.maps.model.GeocodingResult;
 
 public class ejemplo{
-//    GeoApiContext context = new GeoApiContext.Builder()
-//        .apiKey("AIzaSyB88REh9fMvRsTARfNYm5ndpf7vEduamI4")
-//        .build();
-//    GeocodingResult[] results =  GeocodingApi.geocode(context, "1600 Amphitheatre Parkway Mountain View, CA 94043").await();
-//    Gson gson = new GsonBuilder().setPrettyPrinting().create();
-//    System.out.println(gson.toJson(results[0].addressComponents));
+    String llaveApi = "AIzaSyB88REh9fMvRsTARfNYm5ndpf7vEduamI4";
+    GeoApiContext context = new GeoApiContext.Builder().apiKey(llaveApi).build();
+    
+    String direccion = "1600 Amphitheatre Parkway Mountain View, CA 94043";
+    
+    GeocodingResult[] results =  GeocodingApi.geocode(context, direccion).await();
+    Gson gson = new GsonBuilder().setPrettyPrinting().create();
+    System.out.println(gson.toJson(results[0].addressComponents));
+
+    
+    
 }
