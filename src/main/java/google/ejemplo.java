9f82937148ec24da3d619cdcda31fd06f6735662
Gilberto Rodriguez <Gilberto Rodriguez@192.168.1.2>
2018-06-15 09:23:28 -0600

    cofigo ejemplo de la libreria

---
+++ src/main/java/google/ejemplo.java
@@ -0,0 +1,15 @@
+
+package google;
+
+import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
+import com.google.maps.model.GeocodingResult;
+
+public class ejemplo{
+//    GeoApiContext context = new GeoApiContext.Builder()
+//        .apiKey("AIzaSyB88REh9fMvRsTARfNYm5ndpf7vEduamI4")
+//        .build();
+//    GeocodingResult[] results =  GeocodingApi.geocode(context, "1600 Amphitheatre Parkway Mountain View, CA 94043").await();
+//    Gson gson = new GsonBuilder().setPrettyPrinting().create();
+//    System.out.println(gson.toJson(results[0].addressComponents));
+}
