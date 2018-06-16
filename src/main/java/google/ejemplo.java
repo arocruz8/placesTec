603b2f426d21dec6a64c350ea5aa6c63af3a3ff1
Gilberto Rodriguez <Gilberto Rodriguez@192.168.1.2>
2018-06-15 19:17:00 -0600

    trato de hacer la llamada del placesApi

--- src/main/java/google/ejemplo.java
+++ src/main/java/google/ejemplo.java
@@ -5,14 +5,27 @@ import com.google.gson.Gson;
 import com.google.gson.GsonBuilder;
 import com.google.maps.model.GeocodingResult;
 
+import com.google.maps.PlaceDetailsRequest;
+import com.google.maps.TextSearchRequest;
+
 public class ejemplo{
-    String llaveApi = "AIzaSyB88REh9fMvRsTARfNYm5ndpf7vEduamI4";
-    GeoApiContext context = new GeoApiContext.Builder().apiKey(llaveApi).build();
+    //variables para hacer una busqueda de un lugar
+    String ApiKey = "AIzaSyB88REh9fMvRsTARfNYm5ndpf7vEduamI4";
+    int radio = 100;
     
     String direccion = "1600 Amphitheatre Parkway Mountain View, CA 94043";
+    String query = "restaurants+in+Sydney";
+
+    //location ocupa la latittud y longitud
     
-    GeocodingResult[] results =  GeocodingApi.geocode(context, direccion).await();
+    String urlTexto = "https://maps.googleapis.com/maps/api/place/textsearch/xml?query="+query+"&key="+ApiKey;
+   
+    GeoApiContext context = new GeoApiContext.Builder().apiKey(ApiKey).build();
+    
+    PlacesApi[] results = PlacesApi.textSearchQuery(context, query).await();
     Gson gson = new GsonBuilder().setPrettyPrinting().create();
+    
+    
     System.out.println(gson.toJson(results[0].addressComponents));
 
     
